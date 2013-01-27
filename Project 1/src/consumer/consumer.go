2ac5d9aa16cf5838cb05cffa58dc7cdcae304040
Jon Shahen <jon_shahen@yahoo.ca>
2013-01-26 23:54:26 -0500

    Init; Queue is built along with simple test

--- Project 1/src/consumer/consumer.go
+++ Project 1/src/consumer/consumer.go
@@ -2,15 +2,31 @@ package consumer
 
 import (
 	"log"
+	"time"
 )
 
 var logger *log.Logger
+var (
+	packet_len        int     // packet length in bits
+	transmission_rate float32 // bits per second
+	wait_time         int     // in milliseconds
+)
 
-func Init(l *log.Logger) {
+func Init(l *log.Logger, _packet_len int, _transmission_rate float32) {
 	logger = l
 	logger.Println("[Consumer] Started")
+	packet_len = _packet_len
+	transmission_rate = _transmission_rate
+
+	wait_time = int((float32(packet_len) / transmission_rate) * 1000) // in milliseconds
+	logger.Println("[Consumer] Service time is", wait_time, "milliseconds")
 }
 
 func Tick(t int) {
 
+}
+
+func consume_packet() {
+	logger.Println("[Consumer] Consuming Packet for", wait_time, "milliseconds")
+	time.Sleep(time.Duration(wait_time) * time.Millisecond)
 }
---
+++ Project 1/src/consumer/queue.go
@@ -0,0 +1,46 @@
+package consumer
+
+import (
+	// "log"
+	"fmt"
+)
+
+type QueueMgr struct {
+	Size    int        // the current size of the queue
+	MaxSize int        // the maximum size of the queue, 0 for infinite
+	Head    *QueueItem // the head of the queue
+	Tail    *QueueItem // the tail of the queue
+}
+
+type QueueItem struct {
+	Next *QueueItem
+	Data interface{}
+}
+
+func (qm *QueueMgr) Push(item interface{}) error {
+	var qItem = QueueItem{nil, item}
+
+	if qm.Size == 0 {
+		qm.Head = &qItem
+		qm.Tail = &qItem
+		qm.Size++
+	} else if qm.MaxSize == 0 || qm.Size < qm.MaxSize {
+		qm.Tail.Next = &qItem
+		qm.Tail = &qItem
+		qm.Size++
+	} else {
+		return fmt.Errorf("Queue is full, max size %d packets", qm.MaxSize)
+	}
+	return nil
+}
+
+func (qm *QueueMgr) Pop() (interface{}, error) {
+	if qm.Size != 0 {
+		var item interface{}
+		item = qm.Head.Data
+		qm.Head = qm.Head.Next
+		qm.Size--
+		return item, nil
+	}
+	return nil, fmt.Errorf("Queue is full, max size %d packets", qm.MaxSize)
+}
