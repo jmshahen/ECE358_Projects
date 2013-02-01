87a956115dfcb2fa8674717555d409a7bbbbce4a
Kevin Carlton <Kevincarlton777@gmail.com>
2013-02-01 17:28:06 -0500

    fixes #12
    You can now start #14

---
+++ Project 1/question5/nums.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var (
+		lambda  = 250
+		incr    = 50
+		incr_to = 11
+		K       = []int{10, 25, 50}
+		header  = "M,TICKS,TICK Time (1 TICK = X milliseconds),Lambda,L (bits),C (bits per sec),K (zero = infinity)\n"
+		s       string
+		bob     int
+	)
+
+	for i := 0; i < incr_to; i++ {
+		for _, k := range K {
+			bob++
+			s = fmt.Sprintf("5,6e6,1e-6,%d,2000,1.00E+06,%d\n", (lambda + i*incr), k)
+			write_to_file(bob, header, s)
+		}
+	}
+}
+
+func write_to_file(b int, header string, s string) {
+	file, _ := os.Create(fmt.Sprintf("args_%0d.csv", b))
+	file.WriteString(header)
+	file.WriteString(s)
+	file.Close()
+}
