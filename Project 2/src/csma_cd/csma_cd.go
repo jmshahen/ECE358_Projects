eb8a8ab812b0e78892beb38dc98b195500b30629
Jon Shahen <jon_shahen@yahoo.ca>
2013-03-09 17:42:59 -0500

    Joining LAN with CSMA
    taking work from issue #17 to fill the gaps needed for issue #16

--- Project 2/src/csma_cd/csma_cd.go
+++ Project 2/src/csma_cd/csma_cd.go
@@ -1,6 +1,7 @@
 package csma_cd
 
 import (
+	"lan"
 	"log"
 	"math"
 	"math/rand"
@@ -26,28 +27,41 @@ type StateMachine struct {
 }
 
 type CSMA struct {
+	id          int64
 	qm          stats.QueueMgr
 	logger      *log.Logger
 	nextTick    float64
 	curTick     float64
 	lambda      float64
 	time_2_tick float64 //1 tick = X milliseconds
+	lan         *lan.LAN
 
 	state             StateMachine
 	waitFor           int64
-	i                 int
+	i                 int64
 	kmax              int64
 	packet            stats.Packet
 	tp                int64
 	medium_sense_time int64
 }
 
-func (CSMA *csma) Init(l *log.Logger, _lambda float64, TICK_time float64) {
-	csma.logger = l
-	csma.lambda = _lambda
-	csma.qm = stats.QueueMgr
+//Variables that are set by calling func at Init:
+//	logger, lambda, time_2_tick, kmax, tp, medium_sense_time
+func (CSMA *csma) Init(id int64, lan *lan.LAN, logger *log.Logger,
+	_lambda float64, TICK_time float64, kmax int64,
+	tp int64, medium_sense_time int64) {
+
+	csma.id = id
+	csma.logger = logger
 	csma.time_2_tick = TICK_time
-	csma.nextTick = 1
+	csma.lambda = _lambda
+	csma.lan = lan
+	csma.kmax = kmax
+	csma.tp = tp
+	csma.medium_sense_time = medium_sense_time
+
+	csma.qm = stats.QueueMgr
+	csma.nextTick = -1
 	csma.state.state = STATE_START
 
 	logger.Println("[Producer] Started")
@@ -57,6 +71,15 @@ func (CSMA *csma) Init(l *log.Logger, _lambda float64, TICK_time float64) {
 
 func (CSMA *csma) Tick(t int64) {
 	csma.curTick = t
+
+	//produce packets
+	if t >= nextTick {
+		if nextTick != -1 {
+			csma.producePacket(t)
+		}
+		csma.getExpRandNum()
+		csma.logger.Println("[Comp", csma.id, "] Next Tick", csma.nextTick)
+	}
 
 	switch csma.state.state {
 	case STATE_START:
@@ -71,21 +94,23 @@ func (CSMA *csma) Tick(t int64) {
 		fallthrough
 	case STATE_MEDIUM_SENSING:
 		//sense medium
-		//if busy:
-		//		csma.waitFor = t + csma.tp
-		//		goto STATE_STANDARD_WAIT
-		//if csma.waitFor == t goto STATE_TRANSMIT_FRAME
+		if csma.lan.sense_line(csma.id) {
+			csma.waitFor = t + csma.tp
+			csma.state.state = STATE_STANDARD_WAIT
+		} else if csma.waitFor == t {
+			csma.state.state = STATE_TRANSMIT_FRAME
+		}
 	case STATE_STANDARD_WAIT:
 		if csma.waitFor == t {
 			csma.state.state = STATE_MEDIUM_SENSING_INIT
 		}
 	case STATE_TRANSMIT_FRAME:
 		//sense medium
-		//if busy
-		//	collision
-		//	csma.waitFor = t + lan.sendJammingSignal()
-		//	csma.state.state = STATE_JAMMING_SIGNAL
-		if csma.waitFor == t {
+		if csma.lan.sense_line(csma.id) {
+			//collision
+			csma.waitFor = t + csma.lan.send_jam_signal(csma.id, t)
+			csma.state.state = STATE_JAMMING_SIGNAL
+		} else if csma.waitFor == t {
 			csma.state.state = STATE_SUCCESS_SEND
 		}
 	case STATE_JAMMING_SIGNAL:
@@ -104,14 +129,16 @@ func (CSMA *csma) Tick(t int64) {
 		}
 	case STATE_ERROR_SEND:
 		//record failed packet
+		csma.logger.Println(csma.id, "Correctly Sent a Packet")
 		csma.state.state = STATE_START
 	case STATE_SUCCESS_SEND:
 		//record successful packet
+		csma.logger.Println(csma.id, "Failed to send packet", csma.kmax, "times")
 		csma.state.state = STATE_START
 	}
 }
 
-func (CSMA *csma) producePacket(i float64) {
+func (CSMA *csma) producePacket() {
 	var p = stats.Packet{csma.curTick, 0}
 	if err := csma.qm.Push(p); err != nil {
 		//logger.Printf("[Producer] Received Error %v\n", err)
@@ -130,5 +157,5 @@ func (CSMA *csma) getExpRandNum() {
 }
 
 func (CSMA *cs) expBackOff() int64 {
-
+	return rand.Int31n(2**csma.i-1) * csma.tp
 }
